dcc17964cfdc78abc0fe69bfc0e3b6ac952a32ab
BarbaraPruz <barbara.prusiewicz@gmail.com>
2019-04-12 12:06:41 -1000

    Initial Commit - stub app with start of property model

---
+++ app/models/property.rb
@@ -0,0 +1,10 @@
+class Property < ApplicationRecord
+    validates :number, presence: true, uniqueness: true
+    # validates :email, presence: true
+    # validates :full_address, presence: true
+    # validates :square_feet, presence: true
+    # validates :lanai, presence: true
+    # validates :floors, presence: true
+    # validates :frontage, presence: true
+    # validates :view, presence: true  
+end
---
+++ db/migrate/20190412210938_create_properties.rb
@@ -0,0 +1,14 @@
+class CreateProperties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :properties do |t|
+      t.string  :number
+      # t.string  :full_address
+      # t.integer :square_feet
+      # t.integer :lanai
+      # t.integer :floors
+      # t.string  :frontage
+      # t.string  :view
+      t.timestamps
+    end
+  end
+end
